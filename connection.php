5ff2eb6c8a848ddbf7069790fc3860c415fa39f2
akash92rathi <46784457+akash92rathi@users.noreply.github.com>
2019-04-30 02:53:05 +0530

    A web application where we can save our thoughts.
    It is Web application where we can  write down our thoughts.using many web tools like html,css, jvs,boostrap ,php and mysql it is developed. since
    for running this application it needs
     online server.Anyway Using localhost we can run it.

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("localhost", "root", "akash123", "info");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
